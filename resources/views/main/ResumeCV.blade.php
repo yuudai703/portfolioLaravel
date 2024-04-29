5b41bd779de4ff3f7ef80bff4a2e98d8d664f15c
Yuudai_Arai <yuudai703@gmail.com>
2024-04-29 10:45:18 +0900

    styel update

--- resources/views/main/ResumeCV.blade.php
+++ resources/views/main/ResumeCV.blade.php
@@ -1,4 +1,4 @@
-<section class="text-gray-600 body-font cv-sec" _msthidden="12" >
+<section class="text-black body-font cv-sec" _msthidden="12" >
     <h1 class="cv-title" style=" margin-left: 30%; border-bottom: 1px solid #000; width: 400px;">Resume / CV</h1>
     <div class="container px-5 py-24 mx-auto flex flex-wrap" _msthidden="12">
       <div class="flex relative pt-10 pb-20 sm:items-center md:w-2/3 mx-auto" _msthidden="3">
@@ -8,7 +8,7 @@
         <div class="flex-shrink-0 mt-10 sm:mt-0 -ml-3 inline-flex items-center justify-center bg-indigo-500 text-white relative z-3 title-font font-medium text-sm" _msttexthash="4459" _msthidden="1" _msthash="2328" style="height: 50px;width: 50px; border-radius: 50%;">2018年</div>
         <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row" _msthidden="2">
           <div class="flex-grow sm:pl-6 mt-6 sm:mt-0" _msthidden="2">
-            <h2 class="font-medium title-font text-gray-900 mb-1 text-xl" _msttexthash="232921" _msthidden="1" _msthash="2329">現金輸送-機械警備会社に就職</h2>
+            <h2 class="font-medium title-font text-black mb-1 text-xl" _msttexthash="232921" _msthidden="1" _msthash="2329">現金輸送-機械警備会社に就職</h2>
             <p class="leading-relaxed" _msttexthash="6235814" _msthidden="1" _msthash="2330">
               社会人一年目はITとは無関係の仕事をしていました。
             </p>
@@ -22,7 +22,7 @@
         <div class="flex-shrink-0 mt-10 sm:mt-0 -ml-3 inline-flex items-center justify-center bg-indigo-500 text-white relative z-3 title-font font-medium text-sm" _msttexthash="4459" _msthidden="1" _msthash="2328" style="height: 50px;width: 50px; border-radius: 50%;">2020年</div>
         <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row" _msthidden="2">
           <div class="flex-grow sm:pl-6 mt-6 sm:mt-0" _msthidden="2">
-            <h2 class="font-medium title-font text-gray-900 mb-1 text-xl" _msttexthash="204971" _msthidden="1" _msthash="2332">未経験で都内ITベンチャー企業へ転職</h2>
+            <h2 class="font-medium title-font text-black mb-1 text-xl" _msttexthash="204971" _msthidden="1" _msthash="2332">未経験で都内ITベンチャー企業へ転職</h2>
             <p class="leading-relaxed" _msttexthash="6235814" _msthidden="1" _msthash="2333">
             IT業界に興味を持ち始め22歳の時思い切って都内の社員数30名、平均年齢20代のベンチャー企業へ転職。同時に先輩エンジニアからプログラミングについて教わっていました。<br>
             インターホンと連動したセキュリティスマホアプリ
@@ -45,7 +45,7 @@
         <div class="flex-shrink-0 mt-10 sm:mt-0 -ml-3 inline-flex items-center justify-center bg-indigo-500 text-white relative z-3 title-font font-medium text-sm" _msttexthash="4459" _msthidden="1" _msthash="2328" style="height: 50px;width: 50px; border-radius: 50%;">2021年</div>
         <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row" _msthidden="2">
           <div class="flex-grow sm:pl-6 mt-6 sm:mt-0" _msthidden="2">
-            <h2 class="font-medium title-font text-gray-900 mb-1 text-xl" _msttexthash="154011" _msthidden="1" _msthash="2335">地元長野県で受託開発会社へ転職</h2>
+            <h2 class="font-medium title-font text-black mb-1 text-xl" _msttexthash="154011" _msthidden="1" _msthash="2335">地元長野県で受託開発会社へ転職</h2>
             <p class="leading-relaxed" _msttexthash="6235814" _msthidden="1" _msthash="2336">
               23歳の時、地元長野県の企業にも興味を持ち転職しました。<br>
               物流会社-販売管理システム 受託開発 担当業務：詳細設計 開発　テスト　保守運用<br>
